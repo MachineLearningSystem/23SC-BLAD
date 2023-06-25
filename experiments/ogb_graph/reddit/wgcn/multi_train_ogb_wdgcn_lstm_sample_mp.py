cf17d656286bdddcdebdcd53d07bae3afc43aea6
fkh12345 <1739321550@qq.com>
2023-06-25 19:05:33 +0800

    Update multi_train_ogb_wdgcn_lstm_sample_mp.py

--- experiments/ogb_graph/reddit/wgcn/multi_train_ogb_wdgcn_lstm_sample_mp.py
+++ experiments/ogb_graph/reddit/wgcn/multi_train_ogb_wdgcn_lstm_sample_mp.py
@@ -34,7 +34,6 @@ def run_forward(model, time_window_size, world_size, cached_subgraph, cached_fea
 
     # reddit
     # wdgcn-lstm
-    #pipeline_list = [0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     #pipeline_list = [0, -1, 1, -1, 1, -1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,0,0,0,0,0]
     #pipeline_list = [0, -1, 2, -1, 2, -1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     rank_time_window_size = int((time_window_size+1)/2)
